debd1ba0ba4d2288b3ce67915abcab43d2975680
Kai Klingenberg <k.klingenberg@gmail.com>
2013-08-05 17:51:01 -0400

    Export a procedure to generate DDL.

--- dbsync/__init__.py
+++ dbsync/__init__.py
@@ -2,6 +2,16 @@
 Top-level exports, for convenience.
 """
 
-from dbsync.core import is_synched, generate_content_types
+from dbsync.core import (
+    is_synched,
+    generate_content_types,
+    set_engine,
+    get_engine)
+from dbsync.models import Base
 from dbsync import client
 from dbsync import server
+
+
+def create_all():
+    """Issues DDL commands."""
+    Base.metadata.create_all(get_engine())
