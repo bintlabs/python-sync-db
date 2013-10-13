501735a8c7d627c413feae87a0c4ac4a5a5f8671
Kai Klingenberg <k.klingenberg@gmail.com>
2013-10-12 20:14:20 -0400

    Adds license.

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from distutils.core import setup
 
 setup(name='dbsync',
       version='0.1.0',
+      url='https://github.com/bintlabs/python-sync-db',
       author='Bint',
       packages=['dbsync', 'dbsync.client', 'dbsync.server', 'dbsync.messages'],
-      description='Centralized database synchronization for SQLAlchemy',)
+      description='Centralized database synchronization for SQLAlchemy',
+      install_requires=['sqlalchemy==0.8.0'],
+      license='MIT',)
