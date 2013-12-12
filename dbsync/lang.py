8a02f79472fec2abd6c8ed73dd2ef3e5c90aad65
crrojas <crrojasperez@gmail.com>
2013-12-12 17:13:49 -0300

    Update lang.py

--- dbsync/lang.py
+++ dbsync/lang.py
@@ -174,9 +174,6 @@ def lookup(predicate, collection, default=None):
 def mfilter(predicate, lst):
     """Removes the elements in *lst* that don't satisfy *predictate*,
     mutating *lst* (a list or a set)."""
-    for l in lst:
-        if len(l) != 2:
-            print l
     matching = filter(lambda e: not predicate(e), lst)
     for e in matching:
         lst.remove(e)
