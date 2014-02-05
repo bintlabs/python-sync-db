f34b765535c3052a7c718f5cc4d383d59c728610
Cristian Rojas <crrojasperez@gmail.com>
2014-02-05 16:02:36 -0300

    Remove % from secret key generator. This cause errors on python scripts

--- dbsync/utils.py
+++ dbsync/utils.py
@@ -16,7 +16,7 @@ def generate_secret(length=128):
     chars = "0123456789"\
         "abcdefghijklmnopqrstuvwxyz"\
         "ABCDEFGHIJKLMNOPQRSTUVWXYZ"\
-        ".,_-+*@:;[](){}~!?|<>=/\&%$#"
+        ".,_-+*@:;[](){}~!?|<>=/\&$#"
     return "".join(random.choice(chars) for _ in xrange(length))
 
 
