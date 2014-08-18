2d09adfebba058480666411935078a8efd851f63
Kai Klingenberg <k.klingenberg@gmail.com>
2014-08-18 10:58:13 -0400

    Update version and requirements.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(name='dbsync',
-      version='0.3.0',
+      version='0.4.0',
       url='https://github.com/bintlabs/python-sync-db',
       author='Bint',
       packages=['dbsync', 'dbsync.client', 'dbsync.server', 'dbsync.messages'],
       description='Centralized database synchronization for SQLAlchemy',
-      install_requires=['sqlalchemy==0.8.0', 'requests'],
+      install_requires=['sqlalchemy>=0.8.0', 'requests'],
       license='MIT',)
